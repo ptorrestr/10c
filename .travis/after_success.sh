ee2be7d4bc90d1d1ac4460c7bb23ec62eefb007a
Pablo Torres <pablo.torres@insight-centre.org>
2017-03-14 15:48:19 +0000

    remove path line in success

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -4,7 +4,6 @@ set -x -e
 
 # Only publish when a tag is build
 if [[ -n "$TRAVIS_TAG" ]]; then
-  export PATH="$HOME/miniconda/bin:$PATH"
   source activate $MINICONDA_ENV_NAME
   file=$(conda build .conda/ --output)
   anaconda -t $ANACONDA_TOKEN upload $file -u $ANACONDA_USER --force
